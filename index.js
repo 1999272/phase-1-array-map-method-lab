37975d9b19339563de503b54fd38bd2914e7c4c8
Mark_Kamau <mark.wainaina@student.moringaschool.com>
2025-03-22 14:24:02 +0300

    completed array map method

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ const tutorials = [
   'What is NaN and how Can we Check for it',
   'What is the difference between stopPropagation and preventDefault?',
   'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  'What is the difference between == and ===?',
+  'What is the difference between event capturing and bubbling?',
+  'What is JSONP?'
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {
+    return tutorial
+      .split(' ') // Split sentence into words
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize each word
+      .join(' '); 
+  });
+};
+
